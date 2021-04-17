d0eb3e3e87bef6990e1ec54efa420663425cd968
Jon Defilla <jon.defilla@swisscom.com>
2021-04-17 18:54:04 +0200

    Added todo in binarytree tests

--- Tests/BinaryTreeTest.cpp
+++ Tests/BinaryTreeTest.cpp
@@ -19,6 +19,8 @@ inline double roundNumber(double value) {
  * sigma((1/n)*cos(x*n),n,0,20)
  * -sqrt(1-x^2)
  * ((-x^2-4*x+46)^(1/2)-4)
+ * -2*ln(abs(x+1)/abs(x))
+ * ln(abs(x*(x-1)))/(2*x-1)
  */
 class BinaryTreeTest : public QObject {
 Q_OBJECT
