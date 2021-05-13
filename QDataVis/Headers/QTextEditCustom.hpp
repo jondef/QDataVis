341eb18441fa93782c357e67ade7328e27ced35e
Jon Defilla <jon.defilla@swisscom.com>
2021-05-13 20:12:52 +0200

    Added Textedit

---
+++ QDataVis/Headers/QTextEditCustom.hpp
@@ -0,0 +1,15 @@
+//
+// Created by TAADEJOM on 13.05.2021.
+//
+
+#ifndef QDATAVIS_QTEXTEDITCUSTOM_HPP
+#define QDATAVIS_QTEXTEDITCUSTOM_HPP
+
+class QTextEditCustom : public QTextEdit {
+Q_OBJECT
+    using QTextEdit::QTextEdit;
+
+};
+
+
+#endif //QDATAVIS_QTEXTEDITCUSTOM_HPP
