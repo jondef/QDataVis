b2a04486507eca74d42967eaf94183d7330ae7f4
Jon Defilla <jon.defilla@swisscom.com>
2021-05-16 02:14:27 +0200

    Refactor DataSet.cpp

--- QDataVis/Sources/DataSet.cpp
+++ QDataVis/Sources/DataSet.cpp
@@ -3,27 +3,31 @@
 //
 
 #include "DataSet.hpp"
-#include <iostream>
 #include <Eigen/Dense>
 
 using namespace Eigen;
 
-MatrixXd vConcat(MatrixXd A, MatrixXd B);
+/**
+ * Concatenates two matrices vertically. A at the top and B at the bottom
+ */
+MatrixXd vConcat(MatrixXd A, MatrixXd B) {
+    MatrixXd D(A.rows()+B.rows(), A.cols()); // <-- D(A.rows() + B.rows(), ...)
+    D << A, B;
+    return D;
+}
 
 QVector<double> DataSet::regression(int degree) const {
     QSharedPointer<QCPGraphDataContainer> data = graph->data();
-
+    int m = data->size(); // rows
+    int n = degree; // columns
     // xPoints = data(:,1);  %x -> A
     // yPoints = data(:,2); %y -> b
-    MatrixXd xPoints(data->size(), 1); // n rows, 1 column
-    MatrixXd yPoints(data->size(), 1); // n rows, 1 column
-    for (int i = 0; i < data->size(); ++i) {
+    MatrixXd xPoints(m, 1); // n rows, 1 column
+    MatrixXd yPoints(m, 1); // n rows, 1 column
+    for (int i = 0; i < m; ++i) {
         xPoints(i, 0) = data->at(i)->key;
         yPoints(i, 0) = data->at(i)->value;
     }
-
-    int m = data->size(); // rows
-    int n = degree; // columns
 
     // x0 = ones(m,1).*80.3e3;
     MatrixXd x1 = xPoints;
@@ -49,18 +53,10 @@ QVector<double> DataSet::regression(int degree) const {
     MatrixXd R = A;
     MatrixXd Q = Im;
     for (int k = 1; k <= n; ++k) {
-//        std::cout << "LOOP ITERATION: " << k << std::endl;
         MatrixXd x = R.col(k - 1).bottomRows(R.rows() - k + 1); // we wanna show R.rows - k rows from the bottom // fixme: ErrorRowIndexOutOfBounds with 5 data points and degree 10
-        double lambda;
 
-//        std::cout << x << std::endl;
-//        std::cout << x.coeff(k,k) << std::endl;
-        if (x.coeff(k, k) < 0) {
-            lambda = x.norm();
-        } else {
-            lambda = -x.norm();
-        }
-//        MatrixXd e = Im.block(k, k, m, n);
+        double lambda = x.coeff(k, k) < 0 ? x.norm() : -x.norm();
+
         MatrixXd e = Im.col(k - 1).bottomRows(R.rows() - k + 1);
         MatrixXd v = (x - lambda * e) / (x - lambda * e).norm(); // use normalize()
         MatrixXd z = Eigen::MatrixXd::Zero(k - 1, 1);
@@ -69,18 +65,15 @@ QVector<double> DataSet::regression(int degree) const {
         MatrixXd H = Eigen::MatrixXd::Identity(m, m) - (2 * (vConcat(z, v) * vConcat(z, v).transpose()));
 
         Q = Q * H;
-//        std::cout << e << std::endl;
         R = H * R;
     }
-//    std::cout << R << std::endl << "FUUUUUCK" << std::endl << Q;
     // Rückwärtseinsetzen
     MatrixXd y = Q.transpose() * yPoints;
 
     MatrixXd x = Eigen::MatrixXd::Zero(1, n);
     // x(n) = y(n)/R(n,n);
-//    std::cout << "FUCK" << std::endl;
     x.coeffRef(0, n - 1) = y.coeff(n  - 1, 0) / R.coeff(n - 1, n - 1);
-//    std::cout << "FUCK" << std::endl;
+
     for (int i = (n - 1); i >= 1; --i) {
         double s = 0;
         for (int j = i + 1; j <= n; ++j) {
@@ -92,11 +85,4 @@ QVector<double> DataSet::regression(int degree) const {
     }
 
     return QVector<double>(x.data(), x.data() + x.rows() * x.cols());
-}
-
-
-MatrixXd vConcat(MatrixXd A, MatrixXd B) {
-    MatrixXd D(A.rows()+B.rows(), A.cols()); // <-- D(A.rows() + B.rows(), ...)
-    D << A, B;
-    return D;
 }
